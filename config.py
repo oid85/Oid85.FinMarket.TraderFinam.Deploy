98a51867615c0e86a34ccc1084324167c24f6451
oid85 <oid85@mail.ru>
2026-09-22 21:21:21 +0700

    .

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 deploy_path = 'C:\\Deploy'
 
-trade_finam_source_path = 'C:\\Repository\\Oid85.FinMarket.TradeFinam\\Oid85.FinMarket.TradeFinam'
-trade_finam_deploy_directory = 'Oid85.FinMarket.TradeFinam'
-trade_finam_exe_file_name = "Oid85.FinMarket.TradeFinam.WebHost.exe"
-trade_finam_service_name = 'Oid85.FinMarket.TradeFinam'
+trade_finam_source_path = 'C:\\Repository\\Oid85.FinMarket.TraderFinam\\Oid85.FinMarket.TraderFinam'
+trade_finam_deploy_directory = 'Oid85.FinMarket.TraderFinam'
+trade_finam_exe_file_name = "Oid85.FinMarket.TraderFinam.WebHost.exe"
+trade_finam_service_name = 'Oid85.FinMarket.TraderFinam'
 trade_finam_appsettings_file = 'appsettings.json'
 trade_finam_deploy_port = 12101
